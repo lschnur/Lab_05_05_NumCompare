244995f9ed0864fb22136515006c39ddc8f7ce97
schnurluke <schnurle@mail.uc.edu>
2022-09-22 13:28:13 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int num1 =6;
+        int num2 =4;
+        if(num2 == num1)
+            System.out.println("Your numbers are equal");
+        else if(num1 > num2)
+            System.out.println("Number 1 is bigger than number 2");
+        else if(num1 < num2)
+            System.out.println("Number 2 is bigger than number 1");
+
+
+    }
+}
